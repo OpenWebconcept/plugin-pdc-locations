aa8e73f5502c418dfca0105b9cb950800a56c8bf
Mike van den Hoek <mike@yard.nl>
2023-08-30 12:26:43 +0200

    (chore): release version 2.2.1

--- src/Locations/Foundation/Plugin.php
+++ src/Locations/Foundation/Plugin.php
@@ -2,10 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- * The base of the plugin.
- */
-
 namespace OWC\PDC\Locations\Foundation;
 
 use OWC\PDC\Base\Foundation\Plugin as BasePlugin;
@@ -17,16 +13,12 @@ class Plugin extends BasePlugin
 {
     /**
      * Name of the plugin.
-     *
-     * @const string NAME
      */
     public const NAME = 'pdc-locations';
 
     /**
      * Version of the plugin.
      * Used for setting versions of enqueue scripts and styles.
-     *
-     * @const string VERSION
      */
-    public const VERSION = '2.2.0';
+    public const VERSION = '2.2.1';
 }
